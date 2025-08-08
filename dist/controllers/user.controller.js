d784f0655c16e2febdefbac1668852806a19fd34
José Guadalupe Zendejas Sotelo <zendejassotelojose@gmail.com>
2025-08-07 19:25:57 -0600

    feat: se creo el archivo .env y se compilo y probaron endpoints Users

--- dist/controllers/user.controller.js
+++ dist/controllers/user.controller.js
@@ -1,16 +1,9 @@
-import type { Request, Response  } from "express";
-import type { RegisterUserDto, LoginUserDto } from "../dtos/user.dto.ts";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { UserModel } from "../models/user.model.js";
-
-
-
-
 export class UserController {
-
-    static async register(req: Request, res: Response): Promise<Response> {
-        const { email, password }: RegisterUserDto = req.body;
+    static async register(req, res) {
+        const { email, password } = req.body;
         try {
             const hashedPassword = await bcrypt.hash(password, 10);
             const newUser = new UserModel({ email, password: hashedPassword });
@@ -20,11 +13,9 @@ export class UserController {
         catch (error) {
             return res.status(500).json({ message: "Error al registrar usuario", error });
         }
-
     }
-
-    static async login(req: Request, res: Response): Promise<Response> {
-        const { email, password }: LoginUserDto = req.body;
+    static async login(req, res) {
+        const { email, password } = req.body;
         try {
             const user = await UserModel.findOne({ email });
             if (!user) {
@@ -34,12 +25,12 @@ export class UserController {
             if (!isMatch) {
                 return res.status(401).json({ message: "Credenciales inválidas" });
             }
-            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET as string, { expiresIn: "1h" });
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
             return res.status(200).json({ message: "Login exitoso", token });
         }
         catch (error) {
             return res.status(500).json({ message: "Error al iniciar sesión", error });
         }
-
     }
 }
+//# sourceMappingURL=user.controller.js.map
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import user from "./routes/user.route.js";
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use("/api/users", user);
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+    app.listen(PORT, () => {
+        console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    });
+})
+    .catch(err => {
+    console.error("Error al conectar a MongoDB:", err);
+});
+//# sourceMappingURL=index.js.map
